cf067f94e7d935932ebce476f06827e505831dc2
Shane Duffy <shane@shaneduffy.io>
2023-06-24 03:42:59 -0700

    Add IPFS cluster key gen

--- cli.js
+++ cli.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
-const getSwarmKey = require('./index')
-process.stdout.write(getSwarmKey());
+
+import { Command } from 'commander';
+import * as ipfsKeyGen from './index.js';
+
+const program = new Command();
+program
+  .name('ipfs-key-gen')
+  .description('Generate IPFS related keys and secrets.')
+  .version('1.1.0');
+
+program.command('generate')
+  .description('Generate new keys and secrets')
+  .argument('[type]', 'Type of key to generate (swarm-key, cluster-secret)', 'swarm-key')
+  .action((type) => {
+    console.log(type);
+    switch (type) {
+      case 'swarm-key':
+        process.stdout.write(ipfsKeyGen.generateSwarmKey());
+        break;
+      case 'cluster-secret':
+        process.stdout.write(ipfsKeyGen.generateClusterSecret());
+        break;
+    }
+   });
+
+program.parse();
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const os = require('os')
-const crypto = require('crypto')
+import * as os from 'os';
+import * as crypto from 'crypto';
 
-exports = module.exports = function getSwarmKey(){
-    const buf = crypto.randomBytes(32); 
-    return `/key/swarm/psk/1.0.0/${os.EOL}/base16/${os.EOL}${buf.toString('hex')}`;
+export function generateSwarmKey() {
+  const buffer = crypto.randomBytes(32);
+  return `/key/swarm/psk/1.0.0/${os.EOL}/base16/${os.EOL}${buffer.toString(
+    "hex"
+  )}`;
+}
+
+export function generateClusterSecret() {
+  const buffer = crypto.randomBytes(32);
+  return buffer.toString("hex");
 }
